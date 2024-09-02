3732977c260cfbb00e2c0ea2cf83ee13dd313964
AnjaliGrover22 <anjaligrover2210@gmail.com>
2024-09-03 00:54:03 +0200

    orders part including users and products connection done

--- schemas/Order.js
+++ schemas/Order.js
@@ -1,2 +1,30 @@
 const mongoose = require("mongoose");
 const { Schema } = mongoose;
+
+const OrderSchema = new Schema({
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  products: [
+    {
+      productId: {
+        type: Schema.Types.ObjectId,
+        ref: "Product",
+        required: true,
+      },
+      quantity: {
+        type: Number,
+        required: true,
+        default: 1,
+      },
+    },
+  ],
+  total: {
+    type: Number,
+    required: true,
+  },
+});
+
+module.exports = mongoose.model("Order", OrderSchema);
